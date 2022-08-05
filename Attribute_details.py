b07c5629b8cf0a0d5702f95d5bae128e3a5d8b1c
Dharmendra <dharmendra.kantharia@gmail.com>
2022-08-05 23:27:50 +0530

    This is my fist push in git

---
+++ Attribute_details.py
@@ -0,0 +1,7 @@
+import mysql.connector as conn
+
+mydb = conn.connect(host = "localhost",user ="root",passwd = 'Home@123')
+cursor = mydb.cursor()
+cursor.execute("show databases")
+print(cursor.fetchall())
+
